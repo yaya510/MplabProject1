7bf3bdd62f142254285ffdc88a7d6bc8ef893e47
yahya Elfeki <yahyaemad37@gmail.com>
2021-09-11 13:20:39 +0200

    My first Mplab project

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * File:   main.c
+ * Author: yahya
+ *
+ * Created on September 11, 2021, 1:12 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+        
+    }
+}
